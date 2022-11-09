f6578d4f38f55935041691cd03a21d7acbf50843
MJ-Curtido <manu17pc@gmail.com>
2022-11-09 14:13:14 +0100

    V1-empezado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponenteComponent } from './componente/componente.component';
+import { DirectivaDirective } from './directiva.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteComponent,
+    DirectivaDirective
   ],
   imports: [
     BrowserModule
