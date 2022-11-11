23a08c967754d7221ea8caaff66f68f375930aca
MJ-Curtido <manu17pc@gmail.com>
2022-11-11 10:41:01 +0100

    Fin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ComponenteComponent } from './componente/componente.component';
-import { DirectivaDirective } from './directiva.directive';
+import { DirectivaDirective } from './directivas/directiva.directive';
 
 @NgModule({
   declarations: [
